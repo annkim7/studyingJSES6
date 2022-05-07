cded1802a37485fbba0cb0dc9adf25949de89d43
annkim7@github.com <ilc7748@gmail.com>
2022-05-07 21:38:37 +0900

    3-13

---
+++ main.js
@@ -0,0 +1,48 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+
+
+var dino = {
+    x : 10,
+    y : 200,
+    width : 50,
+    height: 50,
+    draw(){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Cactus {
+    constructor(){
+        this.x = 500;
+        this.y = 200;
+        this.width = 50;
+        this.height = 50;
+    }
+    draw(){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+var timer = 0;
+
+function 프레임마다실행할거(){
+    requestAnimationFrame(프레임마다실행할거);
+    timer++;
+
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    if(timer % 20 === 0){
+        var cactus = new Cactus();
+        cactus.draw();
+    }
+    
+    dino.draw();
+}
+프레임마다실행할거();
